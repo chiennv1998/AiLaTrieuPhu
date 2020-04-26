5fc115b43e476fe545cdedef58954d180301e5ef
chiennv1998 <48088722+chiennv1998@users.noreply.github.com>
2020-04-26 09:20:33 +0700

    first commit

---
+++ AiLaTrieuPhu/app/src/main/java/adm/ailatrieuphu/activity/HighScoreActivity.java
@@ -0,0 +1,34 @@
+package adm.ailatrieuphu.activity;
+
+import android.app.ListActivity;
+import android.os.Bundle;
+
+import adm.ailatrieuphu.App;
+import adm.ailatrieuphu.R;
+import adm.ailatrieuphu.adapter.HighScoreAdapter;
+import adm.ailatrieuphu.manager.DatabaseManager;
+
+public class HighScoreActivity extends ListActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_high_score);
+
+        DatabaseManager databaseManager = new DatabaseManager(App.getContext());
+        getListView().setAdapter(new HighScoreAdapter(databaseManager.getHighScore()));
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        App.getMusicPlayer().pauseBgMusic();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        App.getMusicPlayer().resumeBgMusic();
+
+    }
+}
